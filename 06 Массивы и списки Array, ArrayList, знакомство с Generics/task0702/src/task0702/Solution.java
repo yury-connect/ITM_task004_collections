f646a1af218f24ca71b017173bf160ced5f78203
Yury <yury.connect@gmail.com>
2024-09-04 23:25:43 +0300

    solutions: task0702

--- 06 Массивы и списки Array, ArrayList, знакомство с Generics/task0702/src/task0702/Solution.java
+++ 06 Массивы и списки Array, ArrayList, знакомство с Generics/task0702/src/task0702/Solution.java
@@ -2,6 +2,7 @@ package task0702;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /* 
 Массив из строчек в обратном порядке
@@ -18,6 +19,23 @@ Requirements:
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        final int size = 10;
+        final int sizeActual = size - 2;
+        System.out.printf("\nСоздан массив из '%d' элементов. \nЗаполним первые '%d' его элементов.\n", size, sizeActual);
+        String[] arr = new String[size];
+        for(int i = 0; i < sizeActual ; i++) {
+            System.out.printf("\nВведите элемент № '%d' из '%d' : ", i + 1, sizeActual);
+            arr[i] = reader.readLine();
+        }
+
+        System.out.println("Введены следующие значения:");
+        Arrays.stream(arr).forEach(i -> System.out.println(i));
+
+        System.out.println("Вывожу все элементы массива в обратном порядке:");
+        for (int i = size; i > 0; i--) {
+            System.out.println(arr[i - 1]);
+        }
     }
 }
