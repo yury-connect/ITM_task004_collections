e53790b17f5c5b0cdeb2e9dfbeb14dd7b68aec81
Yury <yury.connect@gmail.com>
2024-09-05 23:00:20 +0300

    solutions: task0713

--- 06 Массивы и списки Array, ArrayList, знакомство с Generics/task0713/src/task0713/Solution.java
+++ 06 Массивы и списки Array, ArrayList, знакомство с Generics/task0713/src/task0713/Solution.java
@@ -3,6 +3,7 @@ package task0713;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 /* 
 Играем в Золушку
@@ -25,10 +26,48 @@ Requirements:
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        ArrayList<Integer> src = new ArrayList<>();
+        ArrayList<Integer> div3Items = new ArrayList<>();
+        ArrayList<Integer> div2Items = new ArrayList<>();
+        ArrayList<Integer> noDivItems = new ArrayList<>();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        final int size = 20;
+
+        for (int i = 0; i < size; i++) {
+            System.out.printf("введите элемент № %d из %d (целое число).", i + 1, size);
+            final int current = Integer.parseInt(reader.readLine());
+            src.add(current);
+            boolean isDiv3 = current % 3 == 0;
+            boolean isDiv2 = current % 2 == 0;
+
+            if (isDiv3) {
+                div3Items.add(current);
+            }
+            if (isDiv2) {
+                div2Items.add(current);
+            }
+            if (!isDiv2 && !isDiv3) {
+                noDivItems.add(current);
+            }
+        }
+
+        System.out.println("\nВывожу список базовых элементов:");
+        printList(src);
+
+        System.out.println("\n(x % 3 == 0) элементы:");
+        printList(div3Items);
+
+        System.out.println("\n( x % 2 == 0) элементы:");
+        printList(div2Items);
+
+        System.out.println("\nВсе остальные элементы:");
+        printList(noDivItems);
     }
 
     public static void printList(ArrayList<Integer> list) {
-        //напишите тут ваш код
+        for(Integer item: list) {
+            System.out.println(item);
+        }
     }
 }
