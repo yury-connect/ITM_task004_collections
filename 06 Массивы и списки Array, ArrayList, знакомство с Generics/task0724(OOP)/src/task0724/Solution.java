2069f5feca5d92006670abeac72c510428a51413
Yury <yury.connect@gmail.com>
2024-09-07 08:54:43 +0300

    solutions: task0724

--- 06 Массивы и списки Array, ArrayList, знакомство с Generics/task0724(OOP)/src/task0724/Solution.java
+++ 06 Массивы и списки Array, ArrayList, знакомство с Generics/task0724(OOP)/src/task0724/Solution.java
@@ -2,7 +2,10 @@ package task0724;
 
 /* 
 Семейная перепись
-Создай класс Human с полями имя(String), пол(boolean), возраст(int), отец(Human), мать(Human). Создай объекты и заполни их так, чтобы получилось: Два дедушки, две бабушки, отец, мать, трое детей. Вывести объекты на экран.
+Создай класс Human с полями имя(String), пол(boolean), возраст(int), отец(Human), мать(Human).
+Создай объекты и заполни их так, чтобы получилось:
+Два дедушки, две бабушки, отец, мать, трое детей.
+Вывести объекты на экран.
 
 Примечание:
 Если написать свой метод String toString() в классе Human, то именно он будет использоваться при выводе объекта на экран.
@@ -24,13 +27,62 @@ Requirements:
 
 public class Solution {
     public static void main(String[] args) {
-        // напишите тут ваш код
+        Human grandfatherDad = new Human("Амвросий (дедушка отца)", true, 95); // (String name, boolean sex, int age)
+        Human grandmotherDad = new Human("Василиса (бабушка отца)", false, 90); // (String name, boolean sex, int age)
+        Human grandfatherMom = new Human("Ибрагим (дедушка матери)", false, 87); // (String name, boolean sex, int age)
+        Human grandmotherMom = new Human("Дарья (бабушка матери)", true, 80); // (String name, boolean sex, int age)
+        Human dad = new Human("Иван (отец)", true, 60, grandfatherDad, grandmotherDad); // (String name, boolean sex, int age, Human father, Human mother)
+        Human mom = new Human("Светлана (мать)", false, 55, grandfatherMom, grandmotherMom); // (String name, boolean sex, int age, Human father, Human mother)
+        Human child1 = new Human("Андрей (сын старший)", true, 30, dad, mom); // (String name, boolean sex, int age, Human father, Human mother)
+        Human child2 = new Human("Игорь (сын младший)", true, 19, dad, mom); // (String name, boolean sex, int age, Human father, Human mother)
+        Human child3 = new Human("Наталья (дочь)", false, 17, dad, mom); // (String name, boolean sex, int age, Human father, Human mother)
+
+        System.out.println(grandfatherDad);
+        System.out.println(grandmotherDad);
+        System.out.println(grandfatherMom);
+        System.out.println(grandmotherMom);
+        System.out.println(dad);
+        System.out.println(mom);
+        System.out.println(child1);
+        System.out.println(child2);
+        System.out.println(child3);
     }
 
     public static class Human {
-        // напишите тут ваш код
+        String name;
+        boolean sex;
+        int age;
+        Human father;
+        Human mother;
 
+        public Human(String name, boolean sex, int age, Human father, Human mother) {
+            this.name = name;
+            this.sex = sex; // true - "мужской", false "женский"
+            this.age = age;
+            this.father = father;
+            this.mother = mother;
+        }
+
+        public Human(String name, boolean sex, int age) {
+            this(name, sex, age, null, null);
+        }
+
+        @Override
         public String toString() {
+            StringBuilder result = new StringBuilder();
+            result.append("Имя: " + this.name);
+            result.append(", пол: " + (this.sex ? "мужской" : "женский"));
+            result.append(", возраст: " + this.age);
+
+            if (this.father != null) {
+                result.append(", отец: " + this.father.name);
+            }
+
+            if (this.mother != null) {
+                result.append(", мать: " + this.mother.name);
+            }
+
+            /* Приведенный в задании код заменен кодом выше. Исходный код ниже закомментирован.
             String text = "";
             text += "Имя: " + this.name;
             text += ", пол: " + (this.sex ? "мужской" : "женский");
@@ -43,8 +95,9 @@ public class Solution {
             if (this.mother != null) {
                 text += ", мать: " + this.mother.name;
             }
-
             return text;
+            */
+            return result.toString();
         }
     }
 }
