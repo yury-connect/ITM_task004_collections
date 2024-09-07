ecad7dd3f6eb58b79a9399cdc873e943b4b180c2
Yury <yury.connect@gmail.com>
2024-09-07 19:31:10 +0300

    solutions: task0828

--- 07 Коллекции LinkedList, HashSet, HashMap. Date - дата/task0828(data)/src/task0828/Solution.java
+++ 07 Коллекции LinkedList, HashSet, HashMap. Date - дата/task0828(data)/src/task0828/Solution.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /* 
 Номер месяца
@@ -20,6 +22,30 @@ Requirements:
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(" Вывожу все возможные месяцы года // варианты ввода:");
+        Map<String, Integer> months = generateMonths();
+        months.entrySet().stream().forEach(entry -> System.out.printf("%-15s (месяц № %d)\n", entry.getKey(), entry.getValue()));
+        System.out.println("\n Введите название месяца из предложенных вариантов: ");
+        String month = reader.readLine().toLowerCase().trim();
+        System.out.printf("%s is the %d month.", month, months.get(month));
+    }
+
+    private static Map<String, Integer> generateMonths() {
+        Map<String, Integer> result = new HashMap<>(12);
+        result.put("january", 1);
+        result.put("february", 2);
+        result.put("march", 3);
+        result.put("april", 4);
+        result.put("may", 5);
+        result.put("june", 6);
+        result.put("july", 7);
+        result.put("august", 8);
+        result.put("september", 9);
+        result.put("october", 10);
+        result.put("november", 11);
+        result.put("december", 12);
+        return result;
     }
 }
