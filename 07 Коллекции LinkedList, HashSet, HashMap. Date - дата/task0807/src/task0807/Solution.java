15d36aa350d253d2be007691d66bb327a8d6b4d8
Yury <yury.connect@gmail.com>
2024-09-07 13:37:55 +0300

    solutions: task0807

--- 07 Коллекции LinkedList, HashSet, HashMap. Date - дата/task0807/src/task0807/Solution.java
+++ 07 Коллекции LinkedList, HashSet, HashMap. Date - дата/task0807/src/task0807/Solution.java
@@ -16,16 +16,15 @@ Requirements:
 
 public class Solution {
     public static Object createArrayList() {
-        //напишите тут ваш код
-
+        return new ArrayList<>();
     }
 
     public static Object createLinkedList() {
-        //напишите тут ваш код
-
+        return new LinkedList<>();
     }
 
     public static void main(String[] args) {
-
+        System.out.println(createArrayList());
+        System.out.println(createLinkedList());
     }
 }
